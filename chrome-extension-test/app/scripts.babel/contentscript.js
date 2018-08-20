700dbb8a8183c3a546d9caec2d3d95720d6cdf8a
rafaelbaarbosa <rafaelbarbosamac@gmail.com>
2018-08-20 00:14:51 -0300

    Creates way to analyse each functionality, but its commented for test purposes.

--- chrome-extension-test/app/scripts.babel/contentscript.js
+++ chrome-extension-test/app/scripts.babel/contentscript.js
@@ -7,14 +7,7 @@ let flag = true;
 chrome.runtime.onMessage.addListener( function(request, sender, sendResponse) {
  //  var data = request.data + 'bye' || {};
 
-  // var htmlCollection = document.getElementsByTagName('nav');
-
-  // console.log(htmlCollection[1].querySelectorAll('[href]'));
-
-	// var arr = Array.from(htmlCollection);
-
-	// arr.forEach(logAttributes);
-
+  const funcs = document.querySelectorAll('[uxi-func]');
 
 	const helpAndDoc = eval(request.helpAndDoc);
 	const findAndUndoErrors = eval(request.findAndUndoErrors);
@@ -22,6 +15,22 @@ chrome.runtime.onMessage.addListener( function(request, sender, sendResponse) {
 	const preventErrors = eval(request.preventErrors);
 	const patternsAndFreedom = eval(request.patternsAndFreedom);
 	const flexibility = eval(request.flexibility);
+
+	// for (const func of funcs){
+	// 	console.log(`-------------- Análise da funcionalidade: ${func.getAttribute('uxi-func')}`);
+	// 	console.log('------- Ajuda e documentação -------');
+	// 	helpAndDoc(func);
+	// 	console.log('------- Encontrar e se recuperar de erros -------');
+	// 	findAndUndoErrors(func);
+	// 	console.log('------- Visibilidade do estado do sistema -------');
+	// 	systemState(func);
+	// 	console.log('------- Prevenção de erros -------');
+	// 	preventErrors(func);
+	// 	console.log('------- Flexibilidade e eficiência do uso -------');
+	// 	flexibility(func);
+	// 	console.log('------- Consistência e padronização | Liberdade de controle fácil para o usuário -------');
+	// 	patternsAndFreedom(func);
+	// }
 
 	console.log('------- Ajuda e documentação -------');
 	helpAndDoc(document);
@@ -34,7 +43,7 @@ chrome.runtime.onMessage.addListener( function(request, sender, sendResponse) {
 	console.log('------- Flexibilidade e eficiência do uso -------');
 	flexibility(document);
 	console.log('------- Consistência e padronização | Liberdade de controle fácil para o usuário -------');
-	patternsAndFreedom(document);
+	// patternsAndFreedom(document);
 
 	let data = flag ? 'Todas as imagens possuem ALT.' : 'Essa página possui uma ou mais imagens sem ALT.';
 

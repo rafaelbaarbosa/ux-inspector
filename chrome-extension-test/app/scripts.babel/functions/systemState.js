e7e3666a41a4cf0367df5ae428400ba228989167
rafaelbaarbosa <rafaelbarbosamac@gmail.com>
2018-08-20 00:25:43 -0300

    Analyses actions in systemState function.

--- chrome-extension-test/app/scripts.babel/functions/systemState.js
+++ chrome-extension-test/app/scripts.babel/functions/systemState.js
@@ -13,4 +13,20 @@ const systemState = (domToAnalyse) => {
 	const numberOfActions = domToAnalyse.querySelectorAll('[uxi-primary-action], [uxi-finishing-action], [uxi-cancel-action], [uxi-permanent-action]').length;
 	const feedbackElements = domToAnalyse.querySelectorAll('[uxi-feedback]').length;
 	console.log(`Existem ${numberOfActions} ações e ${feedbackElements} elementos para dar feedback.`);
+
+	// Verifica elementos para finalizar ação
+	const finishingActionElements = domToAnalyse.querySelectorAll('[uxi-finishing-action]').length;
+	console.log(`Existem ${finishingActionElements} elementos que permitem ao usuário finalizar a tarefa principal da funcionalidade em questão.`);
+
+	// Verifica elementos de ação primária
+	const primaryActionElements = domToAnalyse.querySelectorAll('[uxi-primary-action]').length;
+	console.log(`Existem ${primaryActionElements} elementos de ação primária na funcionalidade em questão.`);
+
+	// Verifica elementos de cancelamento
+	const cancelActionElements = domToAnalyse.querySelectorAll('[uxi-cancel-action]').length;
+	console.log(`Existem ${cancelActionElements} elementos de que permitem o usuário cancelar uma ação que ele tenha feito na funcionalidade em questão.`);
+
+	// Verifica elementos de ação permanente
+	const permanentActionElements = domToAnalyse.querySelectorAll('[uxi-permanent-action]').length;
+	console.log(`Existem ${permanentActionElements} elementos de que permitem o usuário realizar uma ação permanent na funcionalidade em questão.`);
 };

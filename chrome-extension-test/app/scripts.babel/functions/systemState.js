6a3333543157d02e9058cbea89c21d23779b3b77
rafaelbaarbosa <rafaelbarbosamac@gmail.com>
2018-08-30 01:37:04 -0300

    Generate html as a result of systemState.

--- chrome-extension-test/app/scripts.babel/functions/systemState.js
+++ chrome-extension-test/app/scripts.babel/functions/systemState.js
@@ -3,30 +3,39 @@
 const systemState = (domToAnalyse) => {
 	// Verifica elementos para visuaizar estado do sistema
 	const systemStateElements = domToAnalyse.querySelectorAll('[uxi-state]').length;
-	console.log(`Existem ${systemStateElements} elementos que permitem a visualiazação do estado do sistema.`);
 	
 	// Verifica se página possui title
+	let pageTitleLi = ``;
 	if (!document.querySelectorAll('title').length)
-		console.log('Página não possui title.');
+		pageTitleLi = `<li>Essa página não possui title.</li>`
 	
 	// Verifica quantos elementos de feedback existem em comparação com o número de ações
 	const numberOfActions = domToAnalyse.querySelectorAll('[uxi-primary-action], [uxi-finishing-action], [uxi-cancel-action], [uxi-permanent-action]').length;
 	const feedbackElements = domToAnalyse.querySelectorAll('[uxi-feedback]').length;
-	console.log(`Existem ${numberOfActions} ações e ${feedbackElements} elementos para dar feedback.`);
 
 	// Verifica elementos para finalizar ação
 	const finishingActionElements = domToAnalyse.querySelectorAll('[uxi-finishing-action]').length;
-	console.log(`Existem ${finishingActionElements} elementos que permitem ao usuário finalizar a tarefa principal da funcionalidade em questão.`);
 
 	// Verifica elementos de ação primária
 	const primaryActionElements = domToAnalyse.querySelectorAll('[uxi-primary-action]').length;
-	console.log(`Existem ${primaryActionElements} elementos de ação primária na funcionalidade em questão.`);
 
 	// Verifica elementos de cancelamento
 	const cancelActionElements = domToAnalyse.querySelectorAll('[uxi-cancel-action]').length;
-	console.log(`Existem ${cancelActionElements} elementos de que permitem o usuário cancelar uma ação que ele tenha feito na funcionalidade em questão.`);
 
 	// Verifica elementos de ação permanente
 	const permanentActionElements = domToAnalyse.querySelectorAll('[uxi-permanent-action]').length;
-	console.log(`Existem ${permanentActionElements} elementos de que permitem o usuário realizar uma ação permanent na funcionalidade em questão.`);
+
+	return (`
+		<h3>Ajuda e documentação</h3>
+		<ul class="alerts-detected">
+			<li>${systemStateElements === 1 ? 'Existe' : 'Existem'} ${systemStateElements} ${systemStateElements === 1 ? 'elemento' : 'elementos'} de visualiazação do estado do sistema.</li>
+			${pageTitleLi}
+			<li>${feedbackElements === 1 ? 'Existe' : 'Existem'} ${feedbackElements} ${feedbackElements === 1 ? 'elemento' : 'elementos'} para dar feedback.</li>
+			<li>${finishingActionElements === 1 ? 'Existe' : 'Existem'} ${finishingActionElements} ${finishingActionElements === 1 ? 'elemento' : 'elementos'} que permitem ao usuário finalizar a tarefa principal da funcionalidade em questão.</li>
+			<li>${primaryActionElements === 1 ? 'Existe' : 'Existem'} ${primaryActionElements} ${primaryActionElements === 1 ? 'elemento' : 'elementos'} de ação primária na funcionalidade em questão.</li>
+			<li>${cancelActionElements === 1 ? 'Existe' : 'Existem'} ${cancelActionElements} ${cancelActionElements === 1 ? 'elemento' : 'elementos'} de cancelar uma ação que o usuário tenha feito na funcionalidade em questão.</li>
+			<li>${permanentActionElements === 1 ? 'Existe' : 'Existem'} ${permanentActionElements} ${permanentActionElements === 1 ? 'elemento' : 'elementos'} de ação permanente na funcionalidade em questão.</li>
+		</ul>
+	`);
+
 };

48b485c3667653cd700e82f9316f3b4bef4d587e
rafaelbaarbosa <rafaelbarbosamac@gmail.com>
2018-08-30 00:41:32 -0300

    Generates html as a result of findAndUndoErrors.

--- chrome-extension-test/app/scripts.babel/functions/findAndUndoErrors.js
+++ chrome-extension-test/app/scripts.babel/functions/findAndUndoErrors.js
@@ -2,8 +2,16 @@
 
 const findAndUndoErrors = (domToAnalyse) => {
 	const errorElements = domToAnalyse.querySelectorAll('[uxi-error]').length;
-	console.log(`Existem ${errorElements} mensagens de erro na página.`);
+	console.log(``);
 
 	const undoErrorsElements = domToAnalyse.querySelectorAll('[uxi-undo-error]').length;
 	console.log(`Existem ${undoErrorsElements} elementos que permitem a recuperação de erros na página.`);
+
+	return (`
+		<h3>Encontrar e se recuperar de erros</h3>
+		<ul class="alerts-detected">
+			<li>Existem ${errorElements} mensagens de erro na funcionalidade.</li>
+			<li>Existem ${undoErrorsElements} elementos que permitem a recuperação de erros na funcionalidade.</li>
+		</ul>
+	`);
 };

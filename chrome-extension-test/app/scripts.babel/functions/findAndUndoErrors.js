bfd9d9911811ff6f6acfcc9a4ee4e6df1064a427
rafaelbaarbosa <rafaelbarbosamac@gmail.com>
2018-08-30 00:42:07 -0300

    Removes console logs.

--- chrome-extension-test/app/scripts.babel/functions/findAndUndoErrors.js
+++ chrome-extension-test/app/scripts.babel/functions/findAndUndoErrors.js
@@ -2,10 +2,7 @@
 
 const findAndUndoErrors = (domToAnalyse) => {
 	const errorElements = domToAnalyse.querySelectorAll('[uxi-error]').length;
-	console.log(``);
-
 	const undoErrorsElements = domToAnalyse.querySelectorAll('[uxi-undo-error]').length;
-	console.log(`Existem ${undoErrorsElements} elementos que permitem a recuperação de erros na página.`);
 
 	return (`
 		<h3>Encontrar e se recuperar de erros</h3>

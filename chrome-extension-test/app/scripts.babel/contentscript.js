fa7b46938381093eaf43f22aad85e4018df38bc2
rafaelbaarbosa <rafaelbarbosamac@gmail.com>
2018-09-11 22:57:51 -0300

    Adds text with the functionalities names. Inspects all the page when there is not any uxi-funcs.

--- chrome-extension-test/app/scripts.babel/contentscript.js
+++ chrome-extension-test/app/scripts.babel/contentscript.js
@@ -18,57 +18,69 @@ chrome.runtime.onMessage.addListener( function(request, sender, sendResponse) {
 	const flexibility = eval(request.flexibility);
 	let promises = [];
 
-	// for (const func of funcs){
-	// 	console.log(`-------------- Análise da funcionalidade: ${func.getAttribute('uxi-func')}`);
-	// 	console.log('------- Ajuda e documentação -------');
-	// 	helpAndDoc(func);
-	// 	console.log('------- Encontrar e se recuperar de erros -------');
-	// 	findAndUndoErrors(func);
-	// 	console.log('------- Visibilidade do estado do sistema -------');
-	// 	systemState(func);
-	// 	console.log('------- Prevenção de erros -------');
-	// 	preventErrors(func);
-	// 	console.log('------- Flexibilidade e eficiência do uso -------');
-	// 	flexibility(func);
-	// 	console.log('------- Consistência e padronização | Liberdade de controle fácil para o usuário -------');
-	// 	patternsAndFreedom(func);
-	// }
-
 	if (!request.getReport) {
-		const helpAndDocReport = helpAndDoc(document);
-		const findAndUndoErrorsReport = findAndUndoErrors(document);
-		const systemStateReport = systemState(document);
-		const preventErrorsReport = preventErrors(document);
-		const flexibilityReport = flexibility(document);
 
-		let promises = [];
-		promises.push(patternsAndFreedom(document));
-		Promise.all(promises).then((results) => {
-			finalReport = `
-				<h2>Relatório de teste :D</h2>
-				<section class="reports">
-				${helpAndDocReport}
-				${findAndUndoErrorsReport}
-				${systemStateReport}
-				${preventErrorsReport}
-				${flexibilityReport}
-				${results[0]}
-				</section>
-			`;
-			sendResponse({data: data, success: true});
-		});
-
-		// finalReport = `
-		// 	<h2>Relatório de funcionalidade teste :D</h2>
-		// 	<section class="reports">
-		// 		${helpAndDocReport}
-		// 		${findAndUndoErrorsReport}
-		// 		${systemStateReport}
-		// 		${preventErrorsReport}
-		// 		${flexibilityReport}
-		// 	</section>
-		// `;
-		// sendResponse({data: data, success: true});
+		if (funcs.length) {
+			let reports = [];
+			let funcsTitles = [];
+			for (const func of funcs){
+				promises.push(patternsAndFreedom(func));
+				funcsTitles.push(func.getAttribute('uxi-func'));
+			}
+	
+			Promise.all(promises).then((results) => {
+	
+				for (let i = 0; i < results.length; i++) {
+					const helpAndDocReport = helpAndDoc(funcs[i]);
+					const findAndUndoErrorsReport = findAndUndoErrors(funcs[i]);
+					const systemStateReport = systemState(funcs[i]);
+					const preventErrorsReport = preventErrors(funcs[i]);
+					const flexibilityReport = flexibility(funcs[i]);
+					const report = `
+						<h2>Relatório de inspeção da funcionalidade ${funcs[i].getAttribute('uxi-func')}</h2>
+						<section class="reports">
+							${helpAndDocReport}
+							${findAndUndoErrorsReport}
+							${systemStateReport}
+							${preventErrorsReport}
+							${flexibilityReport}
+							${results[i]}
+						</section>
+					`;
+					reports.push(report);
+				}
+	
+				finalReport = `
+					<h2>${funcs.length === 1 ? `A funcionalidade ${funcsTitles[0]} foi encontrada e a seguir as questões de usabilidade dela serão apresentadas.` : `Foram encontradas as funcionalidades ${funcsTitles.join(', ')} e a seguir as questões de usabilidade de cada uma delas serão apresentadas.`}</h2>
+					${reports.join('\n')}
+				`;
+				sendResponse({data: data, success: true});
+			});
+		} else {
+			const helpAndDocReport = helpAndDoc(document);
+			const findAndUndoErrorsReport = findAndUndoErrors(document);
+			const systemStateReport = systemState(document);
+			const preventErrorsReport = preventErrors(document);
+			const flexibilityReport = flexibility(document);
+	
+			let promises = [];
+			promises.push(patternsAndFreedom(document));
+			Promise.all(promises).then((results) => {
+				finalReport = `
+					<h2>Não foram encontradas funcionalidades na página, portanto todo o código da página foi tratado como uma funcionalidade.</h2>
+					<h2>Relatório da página</h2>
+					<section class="reports">
+						${helpAndDocReport}
+						${findAndUndoErrorsReport}
+						${systemStateReport}
+						${preventErrorsReport}
+						${flexibilityReport}
+						${results[0]}
+					</section>
+				`;
+				sendResponse({data: data, success: true});
+			});
+		}
 
 	} else {
 		sendResponse({data: data, success: true, finalReport: finalReport});
